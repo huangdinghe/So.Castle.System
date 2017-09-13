3c609e12f092326b4158a44634cf9c285e4e349d
humo <huangdinghe@hotmail.com>
2017-09-13 09:24:23 +0800

    jsoN

--- So.Castle.WebUI/Controllers/UsersController.cs
+++ So.Castle.WebUI/Controllers/UsersController.cs
@@ -11,12 +11,27 @@ namespace So.Castle.WebUI.Controllers
 {
     public class UsersController : BaseController
     {
-        // GET: Users
+        // GET: Users 返回视图
         [HttpGet]
         public ActionResult Index()
         {
             IList<Users> users = Container.Instance.Resolve<IUsersService>().GetAll();
             return View(users);
+        }
+        //返回json数据 返回用户数据
+        [HttpGet]
+        public JsonResult IndexJson()
+        {
+            Users userslist = null;
+            IList<Users> users = Container.Instance.Resolve<IUsersService>().GetAll();
+            if (users != null) userslist = users[0];
+            return Json(new { usersname= userslist.users_name,password=userslist.password},JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult JsonTest(string json)
+        {
+            return Json("humo");
         }
 
         [HttpGet]
